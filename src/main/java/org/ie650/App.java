bed60751df9410d727070d273e9f6c35fe9efde3
Felix Krömer <kroemer.felix@gmail.com>
2022-11-18 21:05:49 +0100

    add quickstart archetype

---
+++ src/main/java/org/ie650/App.java
@@ -0,0 +1,13 @@
+package org.ie650;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
